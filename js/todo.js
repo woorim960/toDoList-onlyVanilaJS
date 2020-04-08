0e53b88f6f1cab0b49f5bcb246d75ce2907d5d69
박우림 <park-woorim@bag-ulim-ui-MacBook-Pro-7.local>
2020-04-09 02:02:54 +0900

    Append Delete Function

--- js/todo.js
+++ js/todo.js
@@ -5,10 +5,34 @@ const toDoForm = document.querySelector('.js-toDoForm'),
 
 const TODOS_LS = 'toDos';
 
+let toDos = [];
+
+function deleteToDo(event) {
+  const btn = event.target.parentNode,
+    li = btn.parentNode;
+  toDoList.removeChild(li);
+
+  const cleanToDos = toDos.filter(function(toDo) {
+    return toDo.id !== parseInt(li.id);
+  });
+  toDos = cleanToDos;
+  saveToDOs();
+  if (toDos.length < 1) box.classList.remove('active');
+}
+
+function saveToDOs() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
 function paintToDo(currentValue) {
   const li = document.createElement('li'),
     delBtn = document.createElement('button'),
     span = document.createElement('span');
+
+  const newId = toDos.length + 1;
+
+  // 삭제 버튼 클릭시 지우기
+  delBtn.addEventListener('click', deleteToDo);
 
   // 박스 div 보이게하기
   box.classList.add('active');
@@ -18,8 +42,16 @@ function paintToDo(currentValue) {
   span.innerText = currentValue;
   li.appendChild(delBtn);
   li.appendChild(span);
+  li.id = newId;
   toDoList.appendChild(li);
+
+  const toDoObj = {
+    id : newId,
+    text : currentValue,
+  };
+  toDos.push(toDoObj);
   
+  saveToDOs();
 }
 
 function handleSubmit(event) {
@@ -30,9 +62,12 @@ function handleSubmit(event) {
 }
 
 function loadToDoList() {
-  const toDos = localStorage.getItem(TODOS_LS);
-  if (toDos !== null) {
-    //
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+  if (loadedToDos !== null) {
+    const parseToDos = JSON.parse(loadedToDos);
+    parseToDos.forEach(function(toDos) {
+      paintToDo(toDos.text);
+    });
   }
 }
 

168163b31588e301b643f73f459ad81bf5b68b1a
박우림 <park-woorim@bag-ulim-ui-MacBook-Pro-7.local>
2020-04-16 01:34:25 +0900

    Append Weather and Country and City

---
+++ js/weather.js
@@ -0,0 +1,59 @@
+const weather = document.querySelector('.js-weather');
+
+const API_KEY = '306a92626af3049f28dffbc9886ff641',
+  COORDS = 'coords';
+
+function getWeather(lat, lon) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+  ).then(function(response) {
+    return response.json();
+  }).then(function(json) {
+    const temp = json.main.temp;
+    const city = json.name;
+    const country = json.sys.country;
+
+    weather.innerText = `국가 : ${country} 도시 : ${city} 온도 : ${temp}`;
+  })
+}
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude
+  }
+  saveCoords(coordsObj);
+  getWeather(latitude, longitude);
+}
+
+function handleGeoError() {
+  console.log("Can't access geo location");
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCoords = localStorage.getItem(COORDS);
+  if(loadedCoords === null) {
+    askForCoords();
+  } else {
+    const parsedCoords = JSON.parse(loadedCoords);
+    getWeather(parsedCoords.latitude, parsedCoords.longitude);
+  }
+}
+
+
+function init() {
+  loadCoords();
+}
+
+
+init();
